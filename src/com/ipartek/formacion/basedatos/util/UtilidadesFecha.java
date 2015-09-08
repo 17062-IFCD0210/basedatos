4d01134713110a468c180d6a6c62a4b0b729a377
aritzrodri <jaguar.wolf@yahoo.es>
2015-09-08 13:20:47 +0200

    fechas

--- src/com/ipartek/formacion/basedatos/util/UtilidadesFecha.java
+++ src/com/ipartek/formacion/basedatos/util/UtilidadesFecha.java
@@ -1,40 +1,52 @@
 package com.ipartek.formacion.basedatos.util;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 
 public class UtilidadesFecha {
-	
-	
+
 	/**
-	 * Convierte una fecha de {@code String} a {@code Timesamp}
-	 * @param sFecha {@code String} formato: "yyyy-mm-ddThh:mm"
+	 * Convierte una fecha en {@code String} a {@code Timestamp}
+	 * @param sFecha {@code String} formato: "yyyy-MM-ddThh:mm"
 	 * @return fecha formateada, null en caso de excepcion
 	 */
-	public static Timestamp stringToTimeStamp(String sFecha){
-		
-		Timestamp resul=null;
-		
+	public static Timestamp stringToTimeStamp( String sFecha ){
+		Timestamp resul = null;
 		try{
-			
-			String fechaModificada=sFecha.replace("T", " ");
-			
-			 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-			    Date parsedDate = (Date) dateFormat.parse(fechaModificada);
-			    resul = new java.sql.Timestamp(parsedDate.getTime());
-		
-		}
-		catch (Exception e){
+			String fechaModificada = sFecha.replace("T"," ");
+			fechaModificada+=":00";
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		    Date parsedDate = dateFormat.parse(fechaModificada);
+		    resul = new java.sql.Timestamp(parsedDate.getTime());
+		}catch(Exception e){
+			e.printStackTrace();
+		}		
+		return resul;
+	}
+	
+	/**
+	 * Convierte una fecha en {@code Timestamp} a {@code String} para usarlo en las JSPs en un input type="datetime-local"	 
+	 * @param tFecha {@code Timestamp} fecha en milisegundos
+	 * @return fecha formateada "yyyy-MM-ddThh:mm" o "" si falla
+	 */
+	public static String timestampToString( Timestamp tFecha ){
+		String resul = "";
+		try{
+		  //yyyy-mm-dd hh:mm:ss.fffffffff	
+		  resul = tFecha.toString();
+		  resul = resul.replace(" ", "T");
+		  resul = resul.substring(0, 16);
+		  
+		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return resul;
-		
-		
 	}
 	
 	
 	
 	
-
 }
